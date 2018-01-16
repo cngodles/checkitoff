29da65b283ea86c41fea77ae1c75f33bf0b13fb4
Clint <cngodles@gmail.com>
2018-01-16 17:07:38 -0500

    Updates
    Find out what today is
    Grab JSON data from file and alter view

--- api.php
+++ api.php
@@ -1,6 +1,17 @@
 <?php
-if($_POST['do'] == 'getmonth'){
-  $jsondata = json_decode(file_get_contents("data-1.json"));
+if(isset($_POST['do'])){
+  if($_POST['do'] == 'getmonth'){
+    header('Content-Type: application/json');
+    $jsondata = json_decode(file_get_contents("data-1.json"));
+    print_r(json_encode($jsondata, JSON_PRETTY_PRINT));
+  }
+  if($_POST['do'] == 'updatedata'){
+    //header('Content-Type: application/json');
+    //$jsondata = json_decode(file_get_contents("data-1.json"));
+    
+    //file_put_contents("data-1.json", json_encode($jsondata));
+  }
+} else {
+  echo 'shooo';
 }
-
 ?>
